0827ed23db0ff0b953625e6f2881565046c5d031
Vincent Bailey <75095846+VMBailey@users.noreply.github.com>
2023-03-20 21:32:18 -0500

    Added options 2 and 3.

--- SurvivalHorror.py
+++ SurvivalHorror.py
@@ -3,50 +3,57 @@
 ##############################################################################
 import pygame, random, time, sys
 
+
 ##############################################################################
 # Classes
 ##############################################################################
-## STARS
-class Redfield (object):
+# STARS
+class Redfield(object):
     health = 150
-    strength = 10
+    strength = 15
     defense = 10
-    resolve = 15
+    resolve = 8
 
-class Valentine (object):
+
+class Valentine(object):
     health = 150
-    strength = 8
+    strength = 18
     defense = 10
-    resolve = 15
-    
-## Enemy
-class Beast (object):
+    resolve = 5
+
+
+# Enemy
+class Beast(object):
     name = "Tyrant"
     health = 40
     strength = 35
     defense = 20
-    loot = random.randint(0,2)
+    loot = random.randint(0, 2)
 
-class Zombie (object):
+
+class Zombie(object):
     name = "Zombie"
     health = 30
     strength = 15
     defense = 20
-    loot = random.randint(0,2)
+    loot = random.randint(0, 2)
 
-class Dog (object):
+
+class Dog(object):
     name = "Cerberus"
     health = 20
     strength = 10
     defense = 20
-    loot = random.randint(0,2)
+    loot = random.randint(0, 2)
 
-class Licker (object):
+
+class Licker(object):
     name = "Licker"
     health = 35
     strength = 20
     defense = 20
-    loot = random.randint(0,2)
+    loot = random.randint(0, 2)
+
 
 ##############################################################################
 # Character Select Function
@@ -63,7 +70,7 @@ def heroselect():
         print("Defense - ", character.defense)
         print("Resolve - ", character.resolve)
         return character
-    
+
     elif selection == "2":
         character = Valentine
         print("Welcome to Arkham Manor, Ms. Valentine. Here are your stats.")
@@ -72,59 +79,63 @@ def heroselect():
         print("Defense - ", character.defense)
         print("Resolve - ", character.resolve)
         return character
-    
+
     elif selection == "3":
         print("Until Next Time.....")
         time.sleep(1.6)
         sys.exit()
-    
+
     else:
         print("Please enter either 1 or 2 to select a character.")
         time.sleep(1.4)
         heroselect()
 
+
 ##############################################################################
 # Enemy Select Function
 ##############################################################################
-def enemyselect(Beast,Zombie,Dog,Licker):
-    enemyList = [Beast,Zombie,Dog,Licker]
-    spawn = random.randint(0,2)
+def enemyselect(Beast, Zombie, Dog, Licker):
+    enemyList = [Beast, Zombie, Dog, Licker]
+    spawn = random.randint(0, 2)
     enemy = enemyList[spawn]
     return enemy
+
 
 ##############################################################################
 # Loot Function
 ##############################################################################
 def loot():
     loot = ["Handgun Bullets", "Combat Knife", "First Aid Spray"]
-    lootChance = random.randint(0,2)
+    lootChance = random.randint(0, 2)
     lootDrop = loot[lootChance]
     return lootDrop
+
 
 ##############################################################################
 # Battle Function
 ##############################################################################
 def battlestate():
-    enemy = enemyselect(Beast,Zombie,Dog,Licker)
+    enemy = enemyselect(Beast, Zombie, Dog, Licker)
     print("A", enemy.name, "lept out at you!")
     time.sleep(1.4)
     print("What are you going to do....")
     while enemy.health > 0:
         choice = input("1. Shoot\n2. Swing Combat Knife\n3. Run\n")
 
+        # Option 1
         if choice == "1":
-            print ("You raise your trusty handgun and fire at the", enemy.name)
-            hitchance = random.randint(0,10)
+            print("You raise your trusty handgun and fire at the", enemy.name)
+            hitchance = random.randint(0, 10)
             if hitchance > 3:
                 enemy.health = enemy.health - character.strength
-                print ("Nice! A solid hit! The", enemy.name, "is now", enemy.health)
+                print("Nice! A solid hit! The", enemy.name, "is now", enemy.health)
 
                 if enemy.health > 0:
                     character.health = character.health - (enemy.strength / character.defense)
-                    print ("The", enemy.name, "strikes at you...")
+                    print("The", enemy.name, "strikes at you...")
                     time.sleep(1.4)
-                    print ("Your health dropped to", character.health)
-                
+                    print("Your health dropped to", character.health)
+
                 else:
                     if enemy.name == "Tyrant":
                         enemy.health = 200
@@ -134,24 +145,87 @@ def battlestate():
 
                     elif enemy.name == "Cerberus":
                         enemy.health = 80
-                    
+
                     elif enemy.name == "Licker":
                         enemy.health = 100
 
-                    print ("Always aim for the head! The", enemy.name, "is down!")
+                    print("Always aim for the head! The", enemy.name, "is down!")
                     time.sleep(1.4)
-                    print ("Looks like it dropped something....")
+                    print("Looks like it dropped something....")
                     time.sleep(1.4)
                     lootDrop = loot()
-                    print ("You picked up", lootDrop, "from the corpse.")
+                    print("You picked up", lootDrop, "from the corpse.")
                     break
 
             else:
-                print ("Damn you missed....")
+                print("Damn you missed....")
                 time.sleep(1.4)
-                print ("The", enemy.name, "hits you for full damage.")
+                print("The", enemy.name, "hits you for full damage.")
                 character.health = character.health - enemy.strength
                 print("Your health is at", character.health, "now.")
+
+        # Option 2
+        if choice == "2":
+            print("You unsheathe your combat knife and take a forceful swing at the", enemy.name)
+            hitchance = random.randint(0, 10)
+            if hitchance > 3:
+                enemy.health = enemy.health - character.resolve
+                print("You cut a deep gash across the", enemy.name, "'s face. It's health is now", enemy.health)
+
+                if enemy.health > 0:
+                    character.health = character.health - (enemy.strength / character.defense)
+                    print("The", enemy.name, "strikes at you...")
+                    time.sleep(1.4)
+                    print("Your health dropped to", character.health)
+
+                else:
+                    if enemy.name == "Tyrant":
+                        enemy.health = 200
+
+                    elif enemy.name == "Zombie":
+                        enemy.health = 100
+
+                    elif enemy.name == "Cerberus":
+                        enemy.health = 80
+
+                    elif enemy.name == "Licker":
+                        enemy.health = 100
+
+                    print("A messy but effective beheading! The", enemy.name, "is down!")
+                    time.sleep(1.4)
+                    print("Looks like it dropped something....")
+                    time.sleep(1.4)
+                    lootDrop = loot()
+                    print("You picked up", lootDrop, "from the corpse.")
+                    break
+
+            else:
+                print("Damn you missed....")
+                time.sleep(1.4)
+                print("The", enemy.name, "hits you for full damage.")
+                character.health = character.health - enemy.strength
+                print("Your health is at", character.health, "now.")
+
+        # Option 3
+        if choice == "3":
+            print("You try to run....")
+            run_chance = random.randint(0, 10)
+            if run_chance > 4:
+                print("You got away unscathed!")
+                break
+            else:
+                print("You try to run but you slip and fall!")
+                time.sleep(1.6)
+                print("You back away quickly but the", enemy.name, "takes a bite out of your chest.")
+                character.health = character.health - enemy.strength
+                time.sleep(1.6)
+                print("You kick the", enemy.name, "off of you and steady yourself.")
+
+        # Any other option
+        else:
+            print("Please select either 1, 2, or 3 to perform an action.")
+
+
 ##############################################################################
 # Main
 ##############################################################################
